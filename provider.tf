1baf4f3b0e11ae66e65a13e0cec4abe56fb8b27a
ImthiYaz Ahamed <46417174+YazJaff@users.noreply.github.com>
2021-12-13 21:20:03 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAUR47PN3XM3FUIV5D"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
